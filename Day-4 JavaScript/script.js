cb430dbc18be4dc74da61934deac6aefe075709a
KSaiSushanth <sushanthsai626@gmail.com>
2026-05-18 22:53:29 +0530

    completed day-4 js fundamentals

---
+++ Day-4 JavaScript/script.js
@@ -0,0 +1,19 @@
+let num1 = 20;
+let num2 = 3;
+let addition = num1 + num2;
+console.log("Addition:",addition);
+let subtraction = num2-num1;
+console.log("subtraction:",subtraction);
+let multiplication = num1*num2;
+console.log("multiplication:",multiplication);
+let division = num1/num2;
+console.log("division:",division);
+let remainder =  num1%num2;
+console.log("remainder:",remainder);
+
+let student = {
+    studentName :"Sai",
+    subject :"Js",
+    marks : 98.34
+}
+console.log(student.studentName + " scored " + student.marks + " in " + student.subject);
